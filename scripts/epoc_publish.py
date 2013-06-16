0576aa77e26ab1a5c493e7709ff71e121a22a079
Robert Chu <robert@wibidata.com>
2013-06-16 14:47:14 -0700

    Adds frame based message publishing.

--- scripts/epoc_publish.py
+++ scripts/epoc_publish.py
@@ -5,9 +5,12 @@ from __future__ import print_function
 # ROS imports.
 import roslib
 import rospy
+from std_msgs.msg import Header
 from std_msgs.msg import UInt32
+from emotiv_epoc.msg import EEGFrame
 
 # Other imports.
+import argparse
 import gevent
 from emokit.emotiv import Emotiv
 
@@ -31,17 +34,18 @@ channels = [
     'Y'
 ]
 
-signal_publishers = {
-    channel: rospy.Publisher('epoc/signal/%s' % channel, UInt32)
-    for channel
-    in channels}
-quality_publishers = {
-    channel: rospy.Publisher('epoc/quality/%s' % channel, UInt32)
-    for channel
-    in channels}
 
+def epoc_publish_channels():
+    # Setup ROS publishers.
+    signal_publishers = {
+        channel: rospy.Publisher('epoc/signal/%s' % channel, UInt32)
+        for channel
+        in channels}
+    quality_publishers = {
+        channel: rospy.Publisher('epoc/quality/%s' % channel, UInt32)
+        for channel
+        in channels}
 
-def epoc_publish():
     # Open a connection to the Emotiv EPOC.
     headset = Emotiv()
     gevent.spawn(headset.setup)
@@ -73,6 +77,73 @@ def epoc_publish():
     except rospy.ROSInterruptException:
         headset.close()
 
+
+def epoc_publish_frames():
+    # Setup ROS publisher.
+    publisher = rospy.Publisher('epoc/frames', EEGFrame)
+
+    # Open a connection to the Emotiv EPOC.
+    headset = Emotiv()
+    gevent.spawn(headset.setup)
+    gevent.sleep(1)
+
+    # Initialize ROS node+publisher.
+    rospy.init_node('epoc_publish')
+
+    # Start the publishing loop.
+    rospy.loginfo('Starting publishing loop...')
+    published_count = 0
+    try:
+        while not rospy.is_shutdown():
+            # Get the next packet from the EPOC.
+            packet = headset.dequeue()
+
+            frame_header = Header(published_count, rospy.Time.now(), '/epoc')
+            frame_accel_x = packet.sensors['X']['value']
+            frame_accel_y = packet.sensors['Y']['value']
+            frame_signals = [
+                packet.sensors[channel]['value']
+                for channel in channels]
+            frame_qualities = [
+                packet.sensors[channel]['quality']
+                for channel in channels]
+            frame = EEGFrame(
+                frame_header,
+                frame_accel_x,
+                frame_accel_y,
+                14,
+                channels,
+                frame_signals,
+                frame_qualities)
+
+            # Publish the the EEG channels and accelerometer values.
+            publisher.publish(frame)
+
+            # Update and print information count.
+            published_count += 1
+            print('\rPublished: %d' % published_count, end='')
+
+            gevent.sleep(0)
+    except rospy.ROSInterruptException:
+        headset.close()
+
+
 if __name__ == '__main__':
     roslib.load_manifest('emotiv_epoc')
-    epoc_publish()
+
+    # Handle cli arguments.
+    parser = argparse.ArgumentParser(
+        description='Publish signals read from the Emotiv EPOC headset.')
+    parser.add_argument(
+        '--message-type',
+        dest='message_type',
+        choices=['frame', 'channel'],
+        default='frame')
+    args = parser.parse_args()
+
+    if args.message_type == 'channel':
+        epoc_publish_channels()
+    elif args.message_type == 'frame':
+        epoc_publish_frames()
+    else:
+        print("Unsupported message_type: %s" % args.message_type)
